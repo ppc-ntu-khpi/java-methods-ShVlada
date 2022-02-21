b06f638c9691cb0ca188c657cee5c09718a18a45
ShVlada <93589382+ShVlada@users.noreply.github.com>
2022-02-21 09:50:06 +0200

    Add files via upload

--- src/TestResult.java
+++ src/TestResult.java
@@ -7,9 +7,9 @@ public class TestResult {
 
     public static void main(String[]args) {
       Scanner in = new Scanner(System.in);
-      System.out.print("Input m: ");
+      System.out.print("Введіть m: ");
       int m = in.nextInt();
-      System.out.print("Input n: ");
+      System.out.print("Введіть n: ");
       int n = in.nextInt();
         System.out.println("Result:"+Exercise.Ackermann(m,n));
       
